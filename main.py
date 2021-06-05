f191e5b77d86f6eae0be78742899526fd163bc82
shou-watanabe <watanabeshou1520@gmail.com>
2021-06-06 00:04:46 +0900

    [add]main

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+from laplacian import laplacian
+
+args = sys.argv
+
+# args: 1:filter_name, 2: image_path ,3: outputDirPath
+filter_name = args[1]
+image_path = args[2]
+output_dir_path = args[3]
+
+# outputフォルダの作成
+if not os.path.exists(output_dir_path):  # ディレクトリがなかったら
+    os.makedirs(output_dir_path)
+
+if filter_name == "laplacian":
+    laplacian(image_path, output_dir_path)
+else:
+    print("filter not found")
